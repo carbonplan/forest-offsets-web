667772a6ae1e5a9196f8b102370142203e8217d9
freeman-lab <the.freeman.lab@gmail.com>
2020-12-17 22:25:31 -0800

    fix url

--- browser/map/style.js
+++ browser/map/style.js
@@ -20,7 +20,7 @@ const style = (locations) => {
       projects: {
         type: 'vector',
         tiles: [
-          `http://storage.googleapis.com/carbonplan-data/tiles/processed/projects_v2/{z}/{x}/{y}.pbf`,
+          `https://storage.googleapis.com/carbonplan-data/tiles/processed/projects_v2/{z}/{x}/{y}.pbf`,
         ],
         maxzoom: 9,
       },
