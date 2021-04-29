f19594814bfad55d68240446ca29f6d25bbe789c
freeman-lab <the.freeman.lab@gmail.com>
2021-04-29 01:54:35 -0700

    new name

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ function Index() {
           textDecoration: 'none',
           mt: [-1, -2, -3, -4],
         }}
-        href='/research/forest-offsets'
+        href='/research/forest-offsets-map'
       >
         <ArrowButton size='xl' fill='green' label='Go to map' />
       </InternalLink>
