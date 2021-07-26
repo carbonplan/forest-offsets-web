43cff0cfc3b35d26bd9d017438c4eaa8bf2d61c3
Joseph Hamman <jhamman@ucar.edu>
2021-07-26 21:55:14 +0000

    add tiles script

--- scripts/active_fires.py
+++ scripts/active_fires.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
 import json
 
+import click
 import fsspec
 import geopandas
+from bs4 import BeautifulSoup
+from fuzzywuzzy import process
 from tenacity import retry, stop_after_attempt
 
 crs = "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
-buffer = 80 * 1000  # m
+
+
+def project_fires_schema():
+    return {"burned_frac": None, "overlapping_fires": []}
+
+
+def fire_schema():
+    return {"name": None, "url": None, "start_date": "", "status": "", "centroid": []}
 
 
 @retry(stop=stop_after_attempt(5))
-def load_offsets_db():
-    with fsspec.open(
-        "https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/forest-offsets-database-v1.0.json"
-    ) as f:
-        projects = json.load(f)
-    return projects
+def get_fire_urls():
+    uri = "https://inciweb.nwcg.gov/accessible-view/"
+
+    with fsspec.open(uri) as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+
+    links_with_text = {}
+    for a in soup.find_all("a", href=True):
+        if a.text and "incident" in a["href"]:
+            links_with_text[a.text] = a["href"]
+    return links_with_text
 
 
 @retry(stop=stop_after_attempt(5))
-def load_project_shape(pid):
-    project_shape = geopandas.read_file(
-        f"https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/projects/{pid}/shape.json"
-    ).to_crs(crs)
-    return project_shape
-
-
-@retry(stop=stop_after_attempt(5))
-def load_fires():
-    # fires
+def get_nifs_fires():
     fires = (
         geopandas.read_file(
             "https://opendata.arcgis.com/api/v3/datasets/2191f997056547bd9dc530ab9866ab61_0/downloads/data?format=geojson&spatialRefId=4326"
@@ -39,44 +45,108 @@ def load_fires():
     return fires
 
 
-def find_fires_near_projects(project_ids, fires):
-    projects_with_fires = []
-    fire_names = []
+@retry(stop=stop_after_attempt(5))
+def get_projects_db():
+    with fsspec.open(
+        "https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/forest-offsets-database-v1.0.json"
+    ) as f:
+        projects = json.load(f)
+    return projects
 
-    for pid in project_ids:
 
-        project_shape = load_project_shape(pid)
-        buffered_project = project_shape.buffer(80 * 1000)
+@retry(stop=stop_after_attempt(5))
+def get_project_shape(pid):
+    project_shape = geopandas.read_file(
+        f"https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/projects/{pid}/shape.json"
+    ).to_crs(crs)
+    return project_shape
 
-        fire_inds = fires.sindex.query(buffered_project.geometry[0], predicate="intersects")
+
+def calc_burned_frac(project_shape, fires):
+
+    project_area = project_shape.area.sum()
+
+    fire_geom = fires.unary_union.buffer(0)
+
+    burned_frac = project_shape.buffer(0).intersection(fire_geom).area.sum() / project_area
+
+    print("burned_frac", burned_frac)
+
+    return burned_frac
+
+
+def find_projects_with_fires(projects, fires_gdf):
+
+    projects_with_fires = {}
+
+    for project in projects:
+        pid = project["id"]
+
+        project_shape = get_project_shape(pid)
+
+        fire_inds = fires_gdf.sindex.query(project_shape.geometry[0], predicate="intersects")
         if fire_inds.size > 0:
-            near_fires = fires["poly_IncidentName"][fire_inds].tolist()
-            projects_with_fires.append(pid)
-            fire_names.extend(near_fires)
-            print(f"found fires near {pid}:", near_fires)
+            project_fires = fires_gdf.loc[fire_inds]
+            print(
+                f"found fire in {pid}:",
+                project_fires["irwin_UniqueFireIdentifier"].tolist(),
+                project_fires["poly_IncidentName"].tolist(),
+            )
+            obj = project_fires_schema()
+            obj["overlapping_fires"] = project_fires["irwin_UniqueFireIdentifier"].tolist()
 
-    fires_subset = fires[fires["poly_IncidentName"].isin(list(set(fire_names)))]
+            obj["burned_frac"] = calc_burned_frac(project_shape, project_fires)
 
-    return fires_subset, projects_with_fires
+            projects_with_fires[pid] = obj
 
 
-def main():
+def make_fires(fire_names_and_urls, fires_gdf):
+    fires = {}
 
-    print("loading projects")
-    projects = load_offsets_db()
-    project_ids = [p["id"] for p in projects]
+    url_keys = list(fire_names_and_urls)
 
-    print("loading fires")
-    fires = load_fires()
+    for i, row in fires_gdf.to_crs("epsg:4326").iterrows():
+        obj = fire_schema()
+        obj["name"] = row["irwin_IncidentName"]
+        match = process.extractOne(row["irwin_IncidentName"], url_keys, score_cutoff=90)
+        if match is not None:
+            obj["url"] = "https://inciweb.nwcg.gov" + fire_names_and_urls[match[0]]
+        obj["start_date"] = row["irwin_FireDiscoveryDateTime"]
+        obj["centroid"] = [row["geometry"].centroid.x, row["geometry"].centroid.y]
 
-    print("finding fires near projects")
-    fires_subset, projects_with_fires = find_fires_near_projects(project_ids, fires)
-    print("all projects with nearby fires: ", projects_with_fires)
+        fires[row["irwin_UniqueFireIdentifier"]] = obj
 
-    print("writing fires to json")
-    # with fsspec.open('gs://carbonplan-scratch/fires-near-offset-projects.json', mode='w') as f:
-    with open("fires-near-offset-projects.json", mode="w") as f:
-        f.write(fires_subset.to_crs("EPSG:4326").to_json())
+    return fires
+
+
+@click.command()
+@click.option("--upload-to", type=str, default=None, help="Where to put the workflow contents")
+def main(upload_to):
+
+    print("loading fire names and urls")
+    fire_names_and_urls = get_fire_urls()
+
+    print("loading nifs fires")
+    fires_gdf = get_nifs_fires()
+
+    print("loading project db")
+    projects = get_projects_db()
+
+    print("making fire metadata")
+    fire_meta = make_fires(fire_names_and_urls, fires_gdf)
+    print("writing fire metadata")
+    with fsspec.open(f"{upload_to}/fire_meta.json", mode="w") as f:
+        json.dump(fire_meta, f)
+
+    print("making projects with fires")
+    projects_with_fires = find_projects_with_fires(projects, fires_gdf)
+    print("writing projects with fires")
+    with fsspec.open(f"{upload_to}/projects_with_fires.json", mode="w") as f:
+        json.dump(projects_with_fires, f)
+
+    print("writing fires.json")
+    with fsspec.open(f"{upload_to}/fires.json", mode="w") as f:
+        f.write(fires_gdf[["irwin_UniqueFireIdentifier", "geometry"]].to_crs("EPSG:4326").to_json())
 
 
 if __name__ == "__main__":
---
+++ scripts/tiles.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+import json
+import os
+import tempfile
+
+import click
+import fsspec
+
+
+def get_projects(tempdir):
+    with fsspec.open(
+        "https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/forest-offsets-database-v1.0.json"
+    ) as f:
+        projects = json.load(f)
+
+    fs = fsspec.get_filesystem_class("http")()
+    for project in projects:
+        pid = project["id"]
+        slug = f"projects/{pid}/shape.json"
+        print(slug, "slug")
+        path = os.path.join(tempdir, f"raw/{slug}")
+        print(path, "path")
+        fs.download(
+            f"https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/{slug}",
+            path,
+        )
+
+
+def get_ecoregions(tempdir):
+    fs = fsspec.get_filesystem_class("http")()
+    for kind in ["baileys", "supersections"]:
+        path = os.path.join(tempdir, f"raw/{kind}.geojson")
+        fs.download(
+            f"https://storage.googleapis.com/carbonplan-data/raw/ecoregions/{kind}.geojson", path
+        )
+
+
+def get_fires(tempdir):
+    fs = fsspec.get_filesystem_class("http")()
+    path = os.path.join(tempdir, "raw/fires.geojson")
+    fs.download("https://storage.googleapis.com/carbonplan-research/offset-fires/fires.json", path)
+
+
+def build_projects(tempdir):
+    cmds = []
+    cmds.append(
+        "tippecanoe "
+        "-z9 "
+        f"-o {tempdir}/tmp/projects.mbtiles "
+        "-l projects "
+        "--no-feature-limit "
+        "--no-tile-size-limit "
+        "--extend-zooms-if-still-dropping "
+        "--no-tile-compression "
+        f"{tempdir}/raw/projects/*/*.json"
+    )
+    cmds.append(
+        "mb-util "
+        "--image_format=pbf "
+        f"{tempdir}/tmp/projects.mbtiles "
+        f"{tempdir}/processed/projects"
+    )
+    [os.system(cmd) for cmd in cmds]
+
+
+def build_ecoregions(tempdir):
+    cmds = []
+    cmds.append(
+        "tippecanoe "
+        "-z5 "
+        f"-o {tempdir}/tmp/ecoregions.mbtiles "
+        "--no-feature-limit "
+        "--no-tile-size-limit "
+        "--extend-zooms-if-still-dropping "
+        "--no-tile-compression "
+        f"{tempdir}/raw/baileys.geojson {tempdir}/raw/supersections.geojson"
+    )
+    cmds.append(
+        "mb-util "
+        "--image_format=pbf "
+        f"{tempdir}/tmp/ecoregions.mbtiles "
+        f"{tempdir}/processed/ecoregions"
+    )
+    [os.system(cmd) for cmd in cmds]
+
+
+def build_fires(tempdir):
+    cmds = []
+    cmds.append(
+        "tippecanoe "
+        "-z5 "
+        f"-o {tempdir}/tmp/fires.mbtiles "
+        "--no-feature-limit "
+        "--no-tile-size-limit "
+        "--extend-zooms-if-still-dropping "
+        "--no-tile-compression "
+        f"{tempdir}/raw/fires.geojson"
+    )
+    cmds.append(
+        "mb-util "
+        "--image_format=pbf "
+        f"{tempdir}/tmp/fires.mbtiles "
+        f"{tempdir}/processed/fires"
+    )
+    [os.system(cmd) for cmd in cmds]
+
+
+def setup_directories(tempdir):
+    os.makedirs(os.path.join(tempdir, "tmp"), exist_ok=True)
+    os.makedirs(os.path.join(tempdir, "processed"), exist_ok=True)
+
+
+def upload_tiles(kind, tempdir, upload_to):
+    fs = fsspec.get_filesystem_class(upload_to.split(":")[0])()
+    lpath = f"{tempdir}/processed/{kind}"
+    rpath = f"{upload_to}/{kind}"
+    fs.put(lpath, rpath, recursive=True)
+
+
+@click.command()
+@click.option("--projects", is_flag=True, help="Build projects tile set")
+@click.option("--ecoregions", is_flag=True, help="Build ecoregion tile set")
+@click.option("--fires", is_flag=True, help="Build fire tile set")
+@click.option("--tempdir", type=str, default=None, help="Temporary directory to use for processing")
+@click.option("--upload-to", type=str, default=None, help="Where to put the workflow contents")
+def main(projects, ecoregions, fires, tempdir, upload_to):
+
+    if tempdir is None:
+        tempdir = tempfile.TemporaryDirectory(suffix="_data")
+
+    print(tempdir)
+    setup_directories(tempdir)
+
+    if projects:
+        get_projects(tempdir)
+        build_projects(tempdir)
+        if upload_to:
+            upload_tiles("projects", tempdir, upload_to)
+    if ecoregions:
+        get_ecoregions(tempdir)
+        build_ecoregions(tempdir)
+        if upload_to:
+            upload_tiles("ecoregions", tempdir, upload_to)
+    if fires:
+        get_fires(tempdir)
+        build_fires(tempdir)
+        if upload_to:
+            upload_tiles("fires", tempdir, upload_to)
+
+
+if __name__ == "__main__":
+    main()
