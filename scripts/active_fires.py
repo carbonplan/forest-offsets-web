25e0cbb2a8e1cc9fdc0bcb9e4b47efa6274189eb
badgley <grayson@carbonplan.org>
2021-09-01 16:45:05 -0400

    changed fire perimeter endpoint to NIFC ArcGIS MapServer; added caching of perimeter data

--- scripts/active_fires.py
+++ scripts/active_fires.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 import datetime
 import json
+import urllib.parse
 
 import click
 import fsspec
 import geopandas
 import git
 import numpy as np
+import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 from fuzzywuzzy import process
 from tenacity import retry, stop_after_attempt
 
-crs = "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
+CRS = "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
 
 # Current datetime
 now = datetime.datetime.utcnow().isoformat()
@@ -49,15 +52,54 @@ def get_fire_urls():
 
 
 @retry(stop=stop_after_attempt(5))
+def get_nifc_perimeter_count():
+    """Return count of perimeters in nifc dataset to enable pagination
+
+    Previously, we used a static download provided by NIFC, which could go stale.
+    Now, we're hitting the underlying database (per NIFC suggestion)
+    """
+    params = {"where": "OBJECTID >0", "returnCountOnly": "true", "f": "pjson"}
+
+    # TODO: what happens when we're in Jan 2022?
+
+    url = "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/CY_WildlandFire_Perimeters_ToDate/FeatureServer/0/query"
+    r = requests.get(url, params=params)
+    return r.json()["count"]
+
+
+def get_fire_url(url):
+    fires = geopandas.read_file(url).to_crs(CRS).reset_index(drop=True)
+    return fires
+
+
+def get_paginated_fire_urls(request_size=1_000):
+    """Generate urls for grabbing all NIFC data.
+
+    Geopandas doesnt support requests-style params, so it's easier to just premake the urls
+    """
+    record_count = get_nifc_perimeter_count()
+    record_offsets = range(1, record_count, request_size)
+
+    base_params = {
+        "f": "geojson",
+        "where": "OBJECTID > 0",
+        "resultRecordCount": request_size,
+        "returnGeometry": "true",
+        "outFields": "*",
+    }
+
+    base_url = "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/CY_WildlandFire_Perimeters_ToDate/FeatureServer/0/query?"
+    urls = [
+        base_url + urllib.parse.urlencode(base_params) + f"&resultOffset={record_offset}"
+        for record_offset in record_offsets
+    ]
+    return urls
+
+
+@retry(stop=stop_after_attempt(5))
 def get_nifs_fires():
-    # source: https://data-nifc.opendata.arcgis.com/datasets/nifc::wfigs-2021-wildland-fire-perimeters-to-date/about
-    fires = (
-        geopandas.read_file(
-            "https://opendata.arcgis.com/datasets/6bd5dd4e93154f6fae0de1f2c82d95bf_0.geojson"
-        )
-        .to_crs(crs)
-        .reset_index()
-    )
+    fire_urls = get_paginated_fire_urls()
+    fires = pd.concat([get_fire_url(fire_url) for fire_url in fire_urls]).reset_index(drop=True)
     return fires
 
 
@@ -74,7 +116,7 @@ def get_projects_db():
 def get_project_shape(pid):
     project_shape = geopandas.read_file(
         f"https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/projects/{pid}/shape.json"
-    ).to_crs(crs)
+    ).to_crs(CRS)
     return project_shape
 
 
@@ -181,26 +223,31 @@ def main(upload_to):
 
     print("making projects with fires")
     projects_with_fires = find_projects_with_fires(projects, fires_gdf)
+
+    print("making fire metadata")
+    fire_meta = make_fires(fire_names_and_urls, fires_gdf, projects_with_fires)
+
     if upload_to:
         print("writing projects with fires")
         with fsspec.open(f"{upload_to}/projects_with_fires.json", mode="w") as f:
             json.dump(projects_with_fires, f)
 
-    print("making fire metadata")
-    fire_meta = make_fires(fire_names_and_urls, fires_gdf, projects_with_fires)
-    if upload_to:
+        print("writing raw NIFC perimeter data")
+        with fsspec.open(
+            f"{upload_to}/fire_perimeter_cache/{now}_raw_nifc_perimeters.parquet", mode="w"
+        ) as f:
+            fires_gdf.to_parquet(f, compression="gzip")
+
         print("writing fire metadata")
         with fsspec.open(f"{upload_to}/fire_meta.json", mode="w") as f:
             json.dump(fire_meta, f)
 
-    if upload_to:
         print("writing fires.json")
         with fsspec.open(f"{upload_to}/fires.json", mode="w") as f:
             f.write(
                 fires_gdf[["irwin_UniqueFireIdentifier", "geometry"]].to_crs("EPSG:4326").to_json()
             )
 
-    if upload_to:
         combined = {"fire_meta": fire_meta, "projects_with_fires": projects_with_fires}
         print("writing combined fire metadata")
         with fsspec.open(f"{upload_to}/fire_meta_combined.json", mode="w") as f:
