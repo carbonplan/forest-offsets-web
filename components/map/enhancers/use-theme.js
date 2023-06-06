36756d74852b3730cb580be933cc2d8c855c8894
katamartin <katasm@gmail.com>
2023-06-06 09:38:13 -0400

    Avoid immediately resetting styles on initial load

--- components/map/enhancers/use-theme.js
+++ components/map/enhancers/use-theme.js
@@ -7,52 +7,39 @@ function useTheme(map, showFires) {
   const { background, muted, red, primary, green } = theme.rawColors
 
   useEffect(() => {
-    map.setPaintProperty('background', 'background-color', background)
-    map.setPaintProperty('background', 'background-opacity', 0)
-    map.setPaintProperty('background', 'background-color', muted)
-    map.setPaintProperty('land', 'fill-opacity', 0)
-    map.setPaintProperty('land', 'fill-color', background)
-    map.setPaintProperty('lakes', 'fill-opacity', 0.2)
-    map.setPaintProperty('lakes', 'fill-color', muted)
-    map.setPaintProperty('countries', 'line-color', primary)
-    map.setPaintProperty('countries', 'line-opacity', 1)
-    map.setPaintProperty('states', 'line-color', primary)
-    map.setPaintProperty('states', 'line-opacity', 0.7)
-    map.setPaintProperty('roads', 'line-color', primary)
-    map.setPaintProperty('roads', 'line-opacity', 0)
-    map.setPaintProperty('projects-fill', 'fill-opacity', 0.5)
-    map.setPaintProperty('projects-line', 'line-color', background)
-    map.setPaintProperty('projects-line', 'line-opacity', 1)
-    map.setPaintProperty('supersections', 'line-color', primary)
-    map.setPaintProperty('supersections', 'line-opacity', 0.2)
-    map.setPaintProperty('projects-label', 'text-opacity', 1)
-    map.setPaintProperty('projects-center', 'circle-opacity', 1)
-    map.setPaintProperty('places-points', 'circle-color', primary)
-    map.setPaintProperty('places-points', 'circle-opacity', 0.3)
-    map.setPaintProperty('places-text', 'text-color', primary)
-    map.setPaintProperty('places-text', 'text-opacity', 0.3)
-    if (showFires) {
-      map.setPaintProperty('fires-label', 'text-halo-color', background)
-      map.setPaintProperty('fires', 'fill-color', red)
-      map.setPaintProperty('fires', 'fill-opacity', 0.7)
-      map.setPaintProperty('projects-fill', 'fill-color', primary)
-      map.setPaintProperty(
-        'projects-center',
-        'circle-color',
-        mix(0.5, primary, background)
-      )
-      map.setPaintProperty('projects-label', 'text-color', primary)
-      map.setPaintProperty('fires-label', 'text-opacity', 1)
-    } else {
-      map.setPaintProperty('projects-fill', 'fill-color', green)
-      map.setPaintProperty(
-        'projects-center',
-        'circle-color',
-        mix(0.5, green, background)
-      )
-      map.setPaintProperty('projects-label', 'text-color', green)
+    // Only try to update colors when style is fully loaded.
+    // In the event of a race condition on initial load, this color update would be a no-op anyway.
+    if (map.isStyleLoaded()) {
+      map.setPaintProperty('background', 'background-color', muted)
+      map.setPaintProperty('land', 'fill-color', background)
+      map.setPaintProperty('lakes', 'fill-color', muted)
+      map.setPaintProperty('countries', 'line-color', primary)
+      map.setPaintProperty('states', 'line-color', primary)
+      map.setPaintProperty('projects-line', 'line-color', background)
+      map.setPaintProperty('supersections', 'line-color', primary)
+      map.setPaintProperty('places-points', 'circle-color', primary)
+      map.setPaintProperty('places-text', 'text-color', primary)
+      map.setPaintProperty('projects-label', 'text-halo-color', background)
+      if (showFires) {
+        map.setPaintProperty('fires-label', 'text-halo-color', background)
+        map.setPaintProperty('fires', 'fill-color', red)
+        map.setPaintProperty('projects-fill', 'fill-color', primary)
+        map.setPaintProperty(
+          'projects-center',
+          'circle-color',
+          mix(0.5, primary, background)
+        )
+        map.setPaintProperty('projects-label', 'text-color', primary)
+      } else {
+        map.setPaintProperty('projects-fill', 'fill-color', green)
+        map.setPaintProperty(
+          'projects-center',
+          'circle-color',
+          mix(0.5, green, background)
+        )
+        map.setPaintProperty('projects-label', 'text-color', green)
+      }
     }
-    map.setPaintProperty('projects-label', 'text-halo-color', background)
   }, [theme, showFires])
 }
 
