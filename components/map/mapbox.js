e3175ddc5211b15fb85255e3e272ce0bdf40b791
Kata Martin <katasm@gmail.com>
2023-07-12 13:16:50 -0700

    Update components/map/mapbox.js
    Co-authored-by: Max Jones <14077947+maxrjones@users.noreply.github.com>

--- components/map/mapbox.js
+++ components/map/mapbox.js
@@ -45,7 +45,7 @@ const Mapbox = ({ locations, tiles, map, setMap, setBounds }) => {
         colormap={colormap}
         clim={[0, 1]}
         source={
-          'https://carbonplan-forest-offsets.s3.us-west-1.amazonaws.com/fires/firms_nrt/current-firms-hotspots.zarr'
+          'https://carbonplan-forest-offsets.s3.us-west-1.amazonaws.com/web/tiles/current-firms-hotspots'
         }
         variable={'active'}
         fillValue={9.969209968386869e36}
